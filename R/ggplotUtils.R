85e66812b0a5b6ca09d8ae314dd537e0e065ee1c
Wyming Lee Pang <wpang@genomatica.com>
2014-10-03 17:27:54 -0700

    added gg_halign() to ggplotUtils

--- R/ggplotUtils.R
+++ R/ggplotUtils.R
@@ -52,3 +52,67 @@ multiplot <- function(..., plotlist=NULL, cols=1, layout=NULL) {
     }
   }
 }
+
+#' Horizontally align ggplots
+#' 
+#' @param ... \code{ggplot} objects as separate arguments or a single \code{list()}
+#'    of ggplot objects to align horizontally
+#' @param heights Height(s) for each plot specified with \link[grid]{unit} as defined 
+#'    with \link[grid]{grid.layout}.  Default is \code{NULL}, heights are evenly
+#'    distributed among plots.
+#' @param plot Logical specifying if the result should be plotted.  Default is 
+#'    \code{FALSE}
+#' 
+#' @details
+#' Uses low level \link{grid} and \link{gridExtra} operations to compare, modify,
+#' and arrange grob widths.
+#' 
+#' @section Credits:
+#' Based on the following StackOverflow Questions:
+#' \link{http://stackoverflow.com/questions/13656642/r-align-plot-areas-in-ggplot}
+#' \link{http://stackoverflow.com/questions/13294952/left-align-two-graph-edges-ggplot}
+#' 
+#' @return
+#' If \code{plot = FALSE}, an \link[gridExtra]{arrangeGrob} object is returned.
+#' 
+#' @seealso
+#' \link[grid]{grid.layout}, \link[gridExtra]{arrangeGrob}, \link[grid]{unit}
+#' 
+#' @import grid gridExtra
+#' @export
+gg_halign = function(..., heights = NULL, plot = FALSE) {
+  plots = list(...)
+  
+  if (length(plots) == 1 && inherits(plots[[1]], 'list')) {
+    # plots supplied as a single list object
+    plots = plots[[1]]
+  }
+  
+  grobs = list()
+  widths = list()
+  
+  for (i in 1:length(plots)) {
+    grobs[[i]] = ggplotGrob(plots[[i]])
+    widths[[i]] = grobs[[i]]$widths[2:5]
+  }
+  
+  maxWidth = do.call(grid::unit.pmax, widths)
+  
+  for (i in 1:length(grobs)) {
+    grobs[[i]]$widths[2:5] = as.list(maxWidth)
+  }
+  
+  args = c(grobs, list(ncol=1))
+  if (!is.null(heights)) {
+    args = c(args, list(heights=heights))
+  }
+  
+  if (plot) {
+    # display the plot immediately
+    do.call(grid.arrange, args)
+  } else {
+    # return an arrangeGrob
+    return(do.call(arrangeGrob, args))
+  }
+  
+}
